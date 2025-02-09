fc1f95e2fb23f7dfd76c1b067288872e6413712b
dzemil karisik <karisikdzemil@gmail.com>
2025-02-09 09:38:22 +0100

    preparing for simple project

---
+++ 01-first-react-project/src/Header.jsx
@@ -0,0 +1,11 @@
+import "./style.css"
+
+export default function Header(){
+
+    return (
+       <div className="header">
+            <h1>React App</h1>
+            <p>Welcome to my first React App</p>
+       </div>
+    )
+}
