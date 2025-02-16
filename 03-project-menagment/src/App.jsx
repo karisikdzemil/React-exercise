3401c5b1f43ea507b72f18c0c11a6087e0ad681f
dzemil karisik <karisikdzemil@gmail.com>
2025-02-16 11:06:44 +0100

    setting up new project

---
+++ 03-project-menagment/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="my-8 text-center text-5xl font-bold bg-red-400">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
--- ToDo-list/src/components/Input.jsx
+++ ToDo-list/src/components/Input.jsx
@@ -1,21 +1,23 @@
 
-import "../index.css";
+// import "../index.css";
 
-export default function Input( {passingFunction } ){
+// export default function Input( {passingFunction } ){
 
-    function inputValueHandler(event){
-        let proba = event.target.closest("div");
-        const input = proba.querySelector("input").value;
-        if(input === ''){
-            alert("You must enter something!");
-        }else{
-            return input;
-        }
-    }
-    return(
-        <div>
-            <input type="text" placeholder="Enter ToDo...?"  className="min-w-100 min-h-10 rounded-sm border-2 border-sky-700 px-5 m-10" />
-            <button onClick={()=>inputValueHandler(passingFunction)} className="min-w-20 min-h-10 rounded-sm text-white bg-sky-600">Add</button>
-        </div>
-    )
-}
+//     function inputValueHandler(event){
+//         let proba = event.target.closest("div");
+//         const input = proba.querySelector("input").value;
+//         if(input === ''){
+//             alert("You must enter something!");
+//         }else{
+//             return input;
+//         }
+//     }
+//     return(
+//         <div>
+//             <input type="text" placeholder="Enter ToDo...?"  className="min-w-100 min-h-10 rounded-sm border-2 border-sky-700 px-5 m-10" />
+//             <button onClick={()=>inputValueHandler(passingFunction)} className="min-w-20 min-h-10 rounded-sm text-white bg-sky-600">Add</button>
+//         </div>
+//     )
+// }
+
+// Whate to fix
