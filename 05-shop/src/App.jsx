01c5f90c28e7a425dee5e3910e8ce0c3739c95d4
dzemil karisik <karisikdzemil@gmail.com>
2025-02-26 22:34:29 +0100

    set cart items in state

--- 05-shop/src/App.jsx
+++ 05-shop/src/App.jsx
@@ -1,15 +1,27 @@
-
-import Header from "./components/Header"
-import Cards from "./components/Cards"
-import "./index.css"
+import { useState } from "react";
+import Header from "./components/Header";
+import Cards from "./components/Cards";
+import "./index.css";
 
 function App() {
+  const [cartElements, setCartElements] = useState([]);
+  function addToCartHandler(event) {
+    setCartElements((prevCartElements) => [
+      ...prevCartElements,
+      {
+        name: event.target.closest("li").querySelector("h1").textContent,
+        price: event.target.closest("li").querySelector("p").textContent,
+      },
+    ]);
+    console.log(cartElements);
+  }
+
   return (
     <div>
       <Header />
-      <Cards />
+      <Cards addToCart={addToCartHandler} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- 05-shop/src/components/Cards.jsx
+++ 05-shop/src/components/Cards.jsx
@@ -1,16 +1,19 @@
-
+import { useRef } from "react";
 import { shopItems } from "../dummyData.js";
 
-export default function Cards() {
-    console.log(shopItems)
+export default function Cards( {addToCart} ) {
+    const productName = useRef();
+    const productPrice = useRef();
+
+
     return (
         <ul className="w-[100%] min-h-[90vh] px-16 flex flex-wrap gap-[20px] justify-center p-5">
             {shopItems.map((el, i) => (
                  <li key={i} className="w-60 h-[35vh] bg-white flex flex-col items-center drop-shadow-xl pb-5">
                  <img className="w-[100%] h-[20vh]" src={el.imageUrl} alt="" />
-                 <h1 className="font-bold p-2 text-xl">{el.name}</h1>
-                 <p className="">Cena: {el.price} RSD</p>
-                 <button className="w-30 h-10 mt-2 border-2 border-gray-700 text-gray-700 hover:bg-red-500 rounded-md">Buy</button>
+                 <h1 ref={productName} className="font-bold p-2 text-xl">{el.name}</h1>
+                 <p ref={productPrice} className="">Cena: {el.price} RSD</p>
+                 <button onClick={addToCart} className="w-30 h-10 mt-2 border-2 border-gray-700 text-gray-700 hover:bg-red-500 rounded-md">Add to cart</button>
              </li>
             ))}
         </ul>
