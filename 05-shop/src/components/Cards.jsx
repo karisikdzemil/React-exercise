4e63718d13ebbeb4f8c0f935b433407ea378d683
dzemil karisik <karisikdzemil@gmail.com>
2025-02-26 13:35:09 +0100

    UI update

--- 05-shop/src/components/Cards.jsx
+++ 05-shop/src/components/Cards.jsx
@@ -10,7 +10,7 @@ export default function Cards() {
                  <img className="w-[100%] h-[20vh]" src={el.imageUrl} alt="" />
                  <h1 className="font-bold p-2 text-xl">{el.name}</h1>
                  <p className="">Cena: {el.price} RSD</p>
-                 <button className="w-30 h-10 mt-2 border-2 border-gray-700 text-gray-700 hover:bg-red-500">Buy</button>
+                 <button className="w-30 h-10 mt-2 border-2 border-gray-700 text-gray-700 hover:bg-red-500 rounded-md">Buy</button>
              </li>
             ))}
         </ul>
--- 05-shop/src/components/Header.jsx
+++ 05-shop/src/components/Header.jsx
@@ -1,9 +1,21 @@
-
-export default function Header(){
-
-    return(
-        <header className="w-[100%] h-[10vh] border-b-2 border-gray-500">
-            <h1 className="text-3xl w-60 p-2 border-2 border-indigo-500 text-indigo-500 mt-4 ml-5">DJEMSI SHOP</h1>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header className="w-[100%] h-[10vh] border-b-2 border-gray-500 flex items-center justify-around">
+      <h1 className="text-3xl w-60 h-15 flex justify-center items-center border-2 border-indigo-500 text-indigo-500">
+        DJEMSI SHOP
+      </h1>
+      <div>
+        <input className="w-120 h-[5vh] border-1 border-gray-600 rounded-md pl-5 mr-5" type="text" placeholder="Search for your favorite..." />
+        <button className="w-35 p-2 bg-gray-400 text-white rounded-md">Search</button>
+      </div>
+      <div>
+        <button className="text-2xl w-40 h-12 p-2 border-2 border-indigo-500 text-indigo-500">
+          Filter
+        </button>
+        <button className="text-2xl w-40 h-12 p-2 border-2 border-indigo-500 text-indigo-500">
+          Cart
+        </button>
+      </div>
+    </header>
+  );
 }
