38c908b319046543f16e7c28b61c07f48ce866bc
dzemil karisik <karisikdzemil@gmail.com>
2025-03-01 17:17:05 +0100

    quiz app again

--- 04-quiz-app/src/components/Question.jsx
+++ 04-quiz-app/src/components/Question.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import  QUESTIONS  from '../questions.js';
 
 export default function Question(){
---
+++ 06-quiz-app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+export default function Header(){
+
+    return(
+        <div className="w-[100%] h-[20vh] flex flex-col items-center p-10">
+            <img className="w-[10%] bg-transparent rounded-[50%]" src="quiz-header-image.png" alt="" />
+            <h1 className="text-5xl text-indigo-200 font-bold mt-5">Quiz App</h1>
+        </div>
+    )
+}
