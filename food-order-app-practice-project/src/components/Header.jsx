d50d9dd0a6c05f1b778f58fa2dd545614b7770e5
dzemil karisik <karisikdzemil@gmail.com>
2025-03-05 13:21:43 +0100

    header

---
+++ food-order-app-practice-project/src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Header (){
+
+    return(
+        <header id="main-header">
+            <div id="title">
+                <img src="logo.jpg" alt="Meal photo" />
+                <h1>REACTFOOD</h1>
+            </div>
+            <button>Cart(<span>0</span>)</button> 
+        </header>
+    )
+}
