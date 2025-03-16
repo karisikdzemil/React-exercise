321e4c4ceee0d5973ca36dd1f6ee049ddeaececb
dzemil karisik <karisikdzemil@gmail.com>
2025-03-16 23:21:57 +0100

    add item logic

--- food-order-app-practice-project/src/store/CartContext.jsx
+++ food-order-app-practice-project/src/store/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react"
+import { createContext, useReducer } from "react"
 
 const createContext = createContext({
     items,
@@ -6,6 +6,37 @@ const createContext = createContext({
     removeItem
 });
 
-function cartContextProvider() {
-    
+function cartReducer(state, action){
+    if(action.type === "ADD_ITEM"){
+        const currentIndex = state.items.findIndex((item) => {
+            return item.id === action.item.id;
+        });
+
+        const updatedItems = [...state.items];
+
+        if(currentIndex > -1){
+            const existingItem = state.items[currentIndex];
+            const updatedItem = {
+                ...existingItem,
+                quantity: existingItem.quantity + 1,
+            };
+            updatedItem[currentIndex] = updatedItem;
+        }else{
+            updatedItems.push({ ...action.item, quantity: 1 });
+        }
+
+        return {...state, items: updatedItems};
+    }
+
+    if(action.type === "REMOVE_ITEM"){
+        // do something
+    }
+    return state;
+}
+
+function cartContextProvider({children}) {
+    useReducer(cartReducer, initState);
+    return(
+        <createContext>{children}</createContext>
+    )
 }
