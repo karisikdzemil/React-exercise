15dfb4aee320ebed37a8f70f3c8fb7e4fb5c0192
dzemil karisik <karisikdzemil@gmail.com>
2025-06-21 13:38:14 +0200

    hooks with typescript

--- typescript-react/src/Person.tsx
+++ typescript-react/src/Person.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 
 interface Props {
-    name: string;
-    age: number;
-    isMarried: boolean;
-  }
-
-export const Person  = ( props: Props ) => {
-    return <div>
-        <p>Name: {props.name}</p>
-        <p>Age: {props.age}</p>
-        <p>This person {props.isMarried ? "is married" : "is single"}</p>
-    </div>
+  name: string;
+  age: number;
+  isMarried: boolean;
 }
+
+export const Person = (props: Props) => {
+    const [personBio, setPersonBio] = useState<string | null>(null);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPersonBio(event.target.value)
+    }
+
+  return ( 
+    <div>
+ <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+      <p>This person {props.isMarried ? "is married" : "is single"}</p>
+    <p>{props.name} Bio: {!personBio ? 'No Bio available' : personBio }</p>
+    <input onChange={handleChange}/>
+    </div>
+  );
+};
